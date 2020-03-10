24aab73232ce6cbcdf71563caaac57f545379a81
saurabhacharya24 <saurabhacharya123@gmail.com>
2020-03-10 17:28:28 +1100

    Added test code to retrieve docs from Firestore
    Renamed endpoint to '/api/testing_endpoint'

--- PHASE_1/API_SourceCode/functions/index.js
+++ PHASE_1/API_SourceCode/functions/index.js
@@ -14,11 +14,22 @@ admin.initializeApp({
 
 const db = admin.firestore()
 
-app.get('/hello-world', async (req, res) => {
+app.get('/api/testing_endpoint', async (req, res) => {
     try {
-        // Testing code to send stuff to Firestore
-        await db.collection('test_collection').doc().create({response: 'Hello World'});
-        return res.status(200).send('Hello World!');
+        // Testing code to send doc to Firestore collection
+        // await db.collection('test_collection').doc().create({response: 'Hello World'});
+        // return res.status(200).send('Hello World!');
+
+        // Testing code to retrieve docs from Firestore
+        let results = [];
+
+        const snapshot = await db.collection('test_collection').get()
+        snapshot.forEach(doc => {
+            results.push(doc.data())
+        });
+
+        return res.status(200).send(results)
+        
     } catch (error) {
         console.log(error);
         return res.status(500).send(error);
