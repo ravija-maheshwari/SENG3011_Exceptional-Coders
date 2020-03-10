bd50ef7c0ab418ee25be12af77a16c8fad151f42
saurabhacharya24 <saurabhacharya123@gmail.com>
2020-03-10 15:24:41 +1100

    Added Initial testing code for Firestore interaction

--- PHASE_1/API_SourceCode/functions/index.js
+++ PHASE_1/API_SourceCode/functions/index.js
@@ -5,8 +5,24 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin: true }));
 
-app.get('/hello-world', (req, res) => {
-    return res.status(200).send('Hello World!');
+var serviceAccount = require("./permissions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://seng3011-859af.firebaseio.com"
+});
+
+const db = admin.firestore()
+
+app.get('/hello-world', async (req, res) => {
+    try {
+        // Testing code to send stuff to Firestore
+        await db.collection('test_collection').doc().create({response: 'Hello World'});
+        return res.status(200).send('Hello World!');
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
 });
 
 exports.app = functions.https.onRequest(app);
