c4f4abd1dc1b1da26079f141cb6658ecb0e7727d
saurabhacharya24 <saurabhacharya123@gmail.com>
2020-03-24 01:27:08 +1100

    Renamed file and added api call helper methods

---
+++ PHASE_1/TestScripts/apiTestHelpers.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch')
+
+// URLs with different params
+const url1 = "https://us-central1-seng3011-859af.cloudfunctions.net/app/api/v1/articles?start_date=2017-05-01T12:00:00&end_date=2020-11-01T12:00:00&keyterms=&location="
+const url2 = "https://us-central1-seng3011-859af.cloudfunctions.net/app/api/v1/articles?start_date=2017-05-01T12:00:00&end_date=2020-11-01T12:00:00&keyterms=corona&location="
+const url3 = "https://us-central1-seng3011-859af.cloudfunctions.net/app/api/v1/articles?start_date=2017-05-01T12:00:00&end_date=2020-11-01T12:00:00&keyterms=zika&location=china"
+
+exports.getResponse1 = async function()  {
+    fetch(url1)
+    .then(res => {
+        return res.json()
+    })
+    .then(json => {
+        return json
+    })
+}
+
+exports.getResponse2 = async function() {
+    fetch(url2)
+    .then(res => {
+        return res.json()
+    })
+    .then(json => {
+        return json
+    })
+}
+
+exports.getResponse3 = async function() {
+    fetch(url3)
+    .then(res => {
+        return res.json()
+    })
+    .then(json => {
+        return json
+    })
+}
