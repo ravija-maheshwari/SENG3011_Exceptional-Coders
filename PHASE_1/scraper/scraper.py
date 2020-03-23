1d87f875129351f0782334bf2aa368238598f45c
Nithin Sudhir <nithinsudhir@gmail.com>
2020-03-23 18:38:14 +1100

    scraper with db

--- PHASE_1/scraper/scraper.py
+++ PHASE_1/scraper/scraper.py
@@ -85,7 +85,9 @@ urls = getURLS()
 diseases = ["unknown", "other", "anthrax cutaneous", "anthrax gastrointestinous", "anthrax inhalation", "botulism", "brucellosis", "chikungunya", "cholera", "coronavirus", "cryptococcosis", "cryptosporidiosis", "crimean-congo haemorrhagic fever", "dengue", "diphteria", "ebola haemorrhagic fever", "ehec (e.coli)", "enterovirus 71 infection", "influenza a/h5n1", "influenza a/h7n9", "influenza a/h9n2", "influenza a/h1n1", "influenza a/h1n2", "influenza a/h3n5", "influenza a/h3n2", "influenza a/h2n2", "hand, foot and mouth disease", "hantavirus", "hepatitis a", "hepatitis b", "hepatitis c",
             "hepatitis d", "hepatitis e", "histoplasmosis", "hiv/aids", "lassa fever", "malaria", "marburg virus disease", "measles", "mers-cov", "mumps", "nipah virus", "norovirus infection", "pertussis", "plague", "pneumococcus pneumonia", "poliomyelitis", "q fever", "rabies", "rift valley fever", "rotavirus infection", "rubella", "salmonellosis", "sars", "shigellosis", "smallpox", "staphylococcal enterotoxin b", "thypoid fever", "tuberculosis", "tularemia", "vaccinia and cowpox", "varicella", "west nile virus", "yellow fever", "yersiniosis", "zika", "legionares", "listeriosis", "monkeypox", "COVID-19"]
 legit_diseases = {"coronavirus": "COVID-19"}
-default_app = firebase_admin.initialize_app()
+cred = firebase_admin.credentials.Certificate(
+    './seng3011-859af-firebase-adminsdk-tbsvx-227c77c920.json')
+default_app = firebase_admin.initialize_app(cred)
 
 # DEBUGGING ONLY REMOVE
 seen = {}
