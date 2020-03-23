defaa336aa82d14f6d367fb8e375f1b3f5b2cd3b
saurabhacharya24 <saurabhacharya123@gmail.com>
2020-03-23 20:31:14 +1100

    Added tests for helpers.js functions

--- PHASE_1/TestScripts/api.test.js
+++ PHASE_1/TestScripts/api.test.js
@@ -11,26 +11,146 @@ let req1 = {
     }
 }
 
-// let req2 = {
-//     query: {
-//         start_date: "2016-10-01T12:00:00",
-//         end_date: "2020-12-01T12:00:00",
-//         location: "",
-//     }
-// }
+let req2 = {
+    query: {
+        start_date: "2016-10-01T12:00:00",
+        end_date: "2020-12-01T12:00:00",
+        location: "",
+    }
+}
 
-// let req3 = {
-//     query: {
-//         start_date: "2016-10-01T12:00:00",
-//         end_date: "2020-12-01T12:00:00",
-//         keyterms: "",
-//         location: ""
-//     }
-// }
+let req3 = {
+    query: {
+        start_date: "2016-10-01T12:00:00",
+        end_date: "2020-12-01T12:00:00",
+        keyterms: "",
+        location: ""
+    }
+}
+
+// Sample Articles
+
+let article1 = {
+    url:"https://www.who.int/csr/don/17-january-2020-novel-coronavirus-japan-exchina/en/",
+    date_of_publication: new Date("2020-10-01 13:00:00"),
+    headline: "Novel Coronavirus - Japan (ex-China)",
+    main_text: "On 15 January 2020, the Ministry of Health, Labour and    Welfare, Japan (MHLW) reported an imported case of laboratory-confirmed 2019-novel coronavirus...",
+    reports: [
+        {
+            event_date: "2020-01-03 xx:xx:xx to 2020-01-15",
+            locations: [
+                {
+                    country: "China",
+                    location: "Wuhan, Hubei Province"
+                },
+                {
+                    country: "Japan",
+                    location: ""
+                }
+            ],
+            diseases: [
+                "2019-nCoV"
+            ],
+            syndromes: [
+                "Fever of unknown Origin"
+            ]
+        }
+    ]
+}
+
+let article2 = {
+    url: "www.who.int/lalala_fake_article",
+    date_of_publication: new Date("2018-12-12 12:50:00"),
+    headline: "Outbreaks in Southern Vietnam",
+    main_text: "Three people infected by what is thought to be H5N1 or H7N9  in Ho Chi Minh city. First infection occurred on 1 Dec 2018, and latest is report on 10 December. Two in hospital, one has recovered. Furthermore, two people with fever and rash infected by an unknown disease.",
+    reports: [
+        {
+            event_date: "2018-12-01 xx:xx:xx to 2018-12-10 xx:xx:xx",
+            locations: [
+                {
+                    country: "Vietnam",
+                    location: "Ho Chi Minh"
+                }
+            ],
+            diseases: [
+                "influenza a/h5n1","influenza a/h7n9"
+            ],
+            syndromes: []
+        },
+        {
+            event_date: "2018-12-01 xx:xx:xx to 2018-12-10 xx:xx:xx",
+            locations: [
+                {
+                    country: "Vietnam",
+                    location: "Ho Chi Minh"
+                }
+            ],
+            diseases: [
+                "unknown"
+            ],
+            syndromes: [
+                "Acute fever and rash"
+            ]
+        }
+    ]
+}
 
 // Test Cases
 
-// Test 1 - Check for missing parameter (location)
-test('checks missing parameter (keyterms) for req1 and returns true', () => {
-    expect(errorChecks.checkMissingQueryParams(req1)).toBe(true)
+// Test 1
+// test('checks missing parameter (keyterms) for req1 and returns true', () => {
+//     expect(errorChecks.checkMissingQueryParams(req1)).toBe(true)
+// })
+
+// Ravija:
+// TODO:
+// Add in multiple tests for the 3 errorChecks function
+
+
+// Tests for keyterms
+test('return value of docHasKeyterm is true', () => {
+    expect(helpers.docHasKeyterm(article2, ['outbreak'])).toBe(true)
+})
+
+test('return value of docHasKeyterm is false', () => {
+    expect(helpers.docHasKeyterm(article2, ['corona'])).toBe(false)
+})
+
+test('return value of docHasKeyterm is true for multiple keyterms', () => {
+    expect(helpers.docHasKeyterm(article1, ['corona', 'fever', 'cough'])).toBe(true)
+})
+
+test('return value of docHasKeyterm is false for empty keyterms', () => {
+    expect(helpers.docHasKeyterm(article1, [])).toBe(false)
+})
+
+// Tests for location
+test('return value of docHasLocation is true', () => {
+    expect(helpers.docHasLocation(article1, "china")).toBe(true)
+})
+
+test('return value of docHasLocation is false', () => {
+    expect(helpers.docHasLocation(article1, "Vietnam")).toBe(false)
+})
+
+test('return value of docHasLocation is false for empty location', () => {
+    expect(helpers.docHasLocation(article2, "")).toBe(false)
+})
+
+// Tests for checking if keyterms is empty
+test('return value of isKeytermsParamEmpty is true', () => {
+    expect(helpers.isKeytermsParamEmpty([])).toBe(true)
+})
+
+test('return value of isKeytermsParamEmpty is false', () => {
+    expect(helpers.isKeytermsParamEmpty(['abc'])).toBe(false)
+})
+
+// Tests for checking if location is empty
+test('return value of isLocationParamEmpty is true', () => {
+    expect(helpers.isLocationParamEmpty("")).toBe(true)
+})
+
+test('return value of isLocationParamEmpty is false', () => {
+    expect(helpers.isLocationParamEmpty("Sydney")).toBe(false)
 })
