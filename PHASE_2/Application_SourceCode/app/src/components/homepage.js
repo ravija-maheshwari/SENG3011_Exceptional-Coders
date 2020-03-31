696ecfd265dbd943daed56aadefe223e50653b54
saurabhacharya24 <saurabhacharya123@gmail.com>
2020-03-31 23:50:19 +1100

    Initial Setup for React app

---
+++ PHASE_2/Application_SourceCode/app/src/components/homepage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Homepage extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <p> Homepage of Project (IN PROGRESS) </p>
+            </div>
+        )
+    }
+}
+
+export default Homepage
---
+++ PHASE_2/Application_SourceCode/app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/index.css';
+import Homepage from './components/homepage';
+
+ReactDOM.render(
+    <Homepage />,
+    document.getElementById('root')
+);
