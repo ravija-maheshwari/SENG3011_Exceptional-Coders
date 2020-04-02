69117f1e64d44d1890dea94a8d84a01b90aa3d08
saurabhacharya24 <saurabhacharya123@gmail.com>
2020-04-02 21:49:44 +1100

    Added locations of all areas in NSW
    These are the areas in https://www.health.nsw.gov.au/Infectious/diseases/Pages/covid-19-lga.aspx

---
+++ PHASE_2/Application_SourceCode/app/src/helpers.js
@@ -0,0 +1,487 @@
+exports.allNswAreas = [
+    {
+        name: "Armidale Regional",
+        lat: -30.5015616,
+        lng: 151.6661993 
+    },
+    {
+        name: "Ballina",
+        lat: -28.8379654,
+        lng: 153.5629067 
+    },
+    {
+        name: "Bellingen",
+        lat: -30.4522423,
+        lng: 152.8979566
+    },    
+    {
+        name: "Berrigan",
+        lat: -35.6658838,
+        lng: 145.8064607 
+    },
+    {
+        name: "Blayney",
+        lat: -33.525572,
+        lng: 149.250803 
+    },
+    {
+        name: "Broken Hill",
+        lat: -31.9539135,
+        lng: 141.4539396 
+    },
+    {
+        name: "Cabonne",
+        lat: -33.1753246,
+        lng: 148.8289611
+    },
+    {
+        name: "Cootamundra-Gundagai Regional",
+        lat: -34.6394159,
+        lng: 148.0258033
+    },
+    {
+        name: "Dungog",
+        lat: -32.4046634,
+        lng: 151.7524146
+    },
+    {
+        name: "Glen Innes Severn",
+        lat: -29.70819059,
+        lng: 151.8877321
+    },
+    {
+        name: "Greater Hume Shire",
+        lat: -35.7236611,
+        lng: 147.1915504
+    },
+    {
+        name: "Griffith",
+        lat: -34.281765,
+        lng: 146.045209
+    },
+    {
+        name: "Hay",
+        lat: -34.500507,
+        lng: 144.8451177
+    },
+    {
+        name: "Hilltops",
+        lat: -34.437226,
+        lng: 148.718257
+    },
+    {
+        name: "Inverell",
+        lat: -29.7627514,
+        lng: 151.1102815
+    },
+    {
+        name: "Kempsey",
+        lat: -31.0604769,
+        lng: 152.8481977
+    },
+    {
+        name: "Kiama",
+        lat: -34.6738221,
+        lng: 150.8443706
+    },
+    {
+        name: "Lachlan",
+        lat: -33.2361364,
+        lng: 146.3875767
+    },
+    {
+        name: "Lithgow",
+        lat: -33.4827345,
+        lng: 150.1570173
+    },
+    {
+        name: "Liverpool Plains",
+        lat: -31.6431494,
+        lng: 150.4892292
+    },
+    {
+        name: "Murrumbidgee",
+        lat: -34.8033133,
+        lng: 146.6147238
+    },
+    {
+        name: "Muswellbrook",
+        lat: -32.2848467,
+        lng: 150.9049638
+    },
+    {
+        name: "Nambucca",
+        lat: -30.666463,
+        lng: 152.7323884
+    },
+    {
+        name: "Narrandera",
+        lat: -34.73798240000001,
+        lng: 146.557684
+    },
+    {
+        name: "Narromine",
+        lat: -32.2327424,
+        lng: 148.2394681
+    },
+    {
+        name: "Singleton",
+        lat: -32.5694741,
+        lng: 151.1788155
+    },
+    {
+        name: "Tenterfield",
+        lat: -29.0575678,
+        lng: 152.0189859
+    },
+    {
+        name: "Upper Hunter Shire",
+        lat: -32.0224523,
+        lng: 150.6749653
+    },
+    {
+        name: "Upper Lachlan Shire",
+        lat: -34.4586364,
+        lng: 149.3800105
+    },
+    {
+        name: "Uralla",
+        lat: -30.64148179,
+        lng: 151.4915632
+    },
+    {
+        name: "Warren",
+        lat: -31.7026474,
+        lng: 147.8161713
+    },
+    {
+        name: "Warrumbungle Shire",
+        lat: -31.5224778,
+        lng: 149.3800105
+    },
+    {
+        name: "Weddin",
+        lat: -33.8837358,
+        lng: 147.9162555
+    },
+    {
+        name: "Wollondilly",
+        lat: -34.1108401,
+        lng: 150.4892292
+    },
+    {
+        name: "Dubbo Regional",
+        lat: -32.2315018,
+        lng: 148.6329645
+    },
+    {
+        name: "Lismore",
+        lat: -28.8093154,
+        lng: 153.2882876
+    },
+    {
+        name: "Mid-Western Regional",
+        lat: -32.5018115,
+        lng: 149.6564494
+    },
+    {
+        name: "Strathfield",
+        lat: -33.8774826,
+        lng: 151.0823602
+    },
+    {
+        name: "Albury",
+        lat: -36.0737293,
+        lng: 146.9135418
+    },
+    {
+        name: "Burwood",
+        lat: -33.8773941,
+        lng: 151.1036621
+    },
+    {
+        name: "Snowy Monaro Regional",
+        lat: -36.2354706,
+        lng: 149.1273779
+    },
+    {
+        name: "Yass Valley",
+        lat: -34.9445333,
+        lng: 148.8289611
+    },
+    {
+        name: "Federation",
+        lat: -35.984510,
+        lng: 146.383533
+    },
+    {
+        name: "Orange",
+        lat: -33.283333,
+        lng: 149.1
+    },
+    {
+        name: "Clarence Valley",
+        lat: -29.878388,
+        lng: 152.6383524
+    },
+    {
+        name: "Goulburn Mulwaree",
+        lat: -34.8812017,
+        lng: 149.7487272
+    },
+    {
+        name: "Bathurst Regional",
+        lat: -33.4192822,
+        lng: 149.5775008
+    },
+    {
+        name: "Bega Valley",
+        lat: -36.6806101,
+        lng: 149.6564494
+    },
+    {
+        name: "Eurobodalla",
+        lat: -35.8894726,
+        lng: 149.9334764
+    },
+    {
+        name: "Queanbeyan-Palerang Regional",
+        lat: -35.3539517,
+        lng: 149.235036
+    },
+    {
+        name: "Wagga Wagga",
+        lat: -35.1081689,
+        lng: 147.3598323
+    },
+    {
+        name: "Coffs Harbour",
+        lat: -30.298612,
+        lng: 153.1093922
+    },
+    {
+        name: "Hunters Hill",
+        lat: -33.8337,
+        lng: 151.14366
+    },
+    {
+        name: "Tamworth Regional",
+        lat: -30.7938249,
+        lng: 150.7679216
+    },
+    {
+        name: "Tweed",
+        lat: -28.3541283,
+        lng: 153.3446649
+    },
+    {
+        name: "Byron",
+        lat: -28.6533749,
+        lng: 153.5334025
+    },
+    {
+        name: "Fairfield",
+        lat: -33.8705856,
+        lng: 150.9564102
+    },
+    {
+        name: "Canada Bay",
+        lat: -33.8671,
+        lng: 151.1139
+    },
+    {
+        name: "Wingecarribee",
+        lat: -34.5479968,
+        lng: 150.3037323
+    },
+    {
+        name: "Shellharbour",
+        lat: -34.583333,
+        lng: 150.866667
+    },
+    {
+        name: "Blue Mountains",
+        lat: -33.409983,
+        lng: 150.3037323
+    },
+    {
+        name: "Hawkesbury",
+        lat: -33.2262828,
+        lng: 150.8609358
+    },
+    {
+        name: "Willoughby",
+        lat: -33.7980572,
+        lng: 151.1927601
+    },
+    {
+        name: "Cessnock",
+        lat: -32.8345098,
+        lng: 151.3624266
+    },
+    {
+        name: "Shoalhaven",
+        lat: -35.0810677,
+        lng: 150.4892292
+    },
+    {
+        name: "Campbelltown",
+        lat: -34.0666148,
+        lng: 150.8195571
+    },
+    {
+        name: "Maitland",
+        lat: -32.7383988,
+        lng: 151.5492182
+    },
+    {
+        name: "Port Macquarie-Hastings",
+        lat: -31.4703285,
+        lng: 152.5443612
+    },
+    {
+        name: "Camden",
+        lat: -34.054444,
+        lng: 150.695833
+    },
+    {
+        name: "Lane Cove",
+        lat: -33.8148145,
+        lng: 151.1663935
+    },
+    {
+        name: "Mosman",
+        lat: -33.829075,
+        lng: 151.24409
+    },
+    {
+        name: "Port Stephens",
+        lat: -32.7684603,
+        lng: 151.8409214
+    },
+    {
+        name: "Liverpool",
+        lat: -33.9128955,
+        lng: 150.7446771
+    },
+    {
+        name: "Mid-Coast",
+        lat: -32.0713929,
+        lng: 151.8877321
+    },
+    {
+        name: "Georges River",
+        lat: -34.0494645,
+        lng: 150.8953368
+    },
+    {
+        name: "Bayside",
+        lat: -33.933474,
+        lng: 151.179375
+    },
+    {
+        name: "Lake Macquarie",
+        lat: -33.0311225,
+        lng: 151.5603244
+    },
+    {
+        name: "Parramatta",
+        lat: -33.8135571,
+        lng: 151.003407
+    },
+    {
+        name: "Hornsby",
+        lat: -33.7046328,
+        lng: 151.0987055
+    },
+    {
+        name: "North Sydney",
+        lat: -33.83965,
+        lng: 151.20541
+    },
+    {
+        name: "Wollongong",
+        lat: -34.4278121,
+        lng: 150.8930607
+    },
+    {
+        name: "Newcastle",
+        lat: -32.9282712,
+        lng: 151.7816802
+    },
+    {
+        name: "Ku-ring-gai",
+        lat: -33.7416052,
+        lng: 151.1519716
+    },
+    {
+        name: "Cumberland",
+        lat: -33.8890304,
+        lng: 151.2016178
+    },
+    {
+        name: "Penrith",
+        lat: -33.7506759,
+        lng: 150.687674
+    },
+    {
+        name: "The Hills Shire",
+        lat: -33.6048582,
+        lng: 150.9772841
+    },
+    {
+        name: "Inner West",
+        lat: -33.8584827,
+        lng: 151.1286686
+    },
+    {
+        name: "Randwick",
+        lat: -33.9131545,
+        lng: 151.2420352
+    },
+    {
+        name: "Ryde",
+        lat: -33.8157466,
+        lng: 151.1030164
+    },
+    {
+        name: "Sutherland Shire",
+        lat: -34.0640771,
+        lng: 151.0937206
+    },
+    {
+        name: "Canterbury-Bankstown",
+        lat: -33.9454207,
+        lng: 151.051286
+    },
+    {
+        name: "Woollahra",
+        lat: -33.8864746,
+        lng: 151.2422139
+    },
+    {
+        name: "Blacktown",
+        lat: -33.7668069,
+        lng: 150.905298
+    },
+    {
+        name: "Central Coast",
+        lat: -33.3208415,
+        lng: 151.2335592
+    },
+    {
+        name: "Sydney",
+        lat: -33.8688197,
+        lng: 151.2092955
+    },
+    {
+        name: "Northern Beaches",
+        lat: -33.6759923,
+        lng: 151.2568777
+    },
+    {
+        name: "Waverley​",
+        lat: -33.899693,
+        lng: 151.2554202
+    }
+]
