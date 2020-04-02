0576ae1551cbd887127b580f73214e01d8a92943
Nithin Sudhir <nithinsudhir@gmail.com>
2020-04-02 20:02:34 +1100

    update firestore

--- PHASE_1/scraper/healthscraper.py
+++ PHASE_1/scraper/healthscraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from requests import get
-from firebase_admin import credentials, firestore
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 url = "https://www.health.nsw.gov.au/Infectious/diseases/Pages/covid-19-lga.aspx?fbclid=IwAR1B-9uKNvIPDLvn9viZy5e80bwLX7g67RHNBLrAlfwihifICsT2anFPwMI"
 page = get(url)
@@ -12,8 +14,8 @@ for row in table.find_all('tr'):
     try:
         loc = row.find(class_='moh-rteTableEvenCol-6')
         count = row.find(class_='moh-rteTableOddCol-6')
-        loc_str = loc.get_text().strip().encode()
-        count_str = count.get_text().strip().encode()
+        loc_str = loc.get_text().strip()
+        count_str = count.get_text().strip()
         if loc_str not in cases:
             cases[loc_str] = count_str
         # print(loc_str)
@@ -27,6 +29,10 @@ default_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 for case in cases.keys():
+    # location = str(cases[case])
+    # case = str(case)
+    # print(case, type(case))
+    # print(case, location)
     db.collection(u'cases').document(case).set(cases[case])
     # print(case)
     # print(cases[case])
