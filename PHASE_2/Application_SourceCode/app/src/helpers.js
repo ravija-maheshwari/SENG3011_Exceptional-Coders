939dfb29f9ad8d7b89c9dbd0cc357f26980550d3
saurabhacharya24 <saurabhacharya123@gmail.com>
2020-04-03 21:10:34 +1100

    Fixed "weird" bug with returning radius

--- PHASE_2/Application_SourceCode/app/src/helpers.js
+++ PHASE_2/Application_SourceCode/app/src/helpers.js
@@ -487,18 +487,22 @@ exports.allNswAreas = [
 ]
 
 //Returns the radius of the display circle on the map
-exports.getRadius = function(suburbCases,  suburb){
-    suburbCases.forEach(suburbCase => {
-        if(suburb.name == suburbCase.name){
-            let caseCount = getIntegerCases(suburbCase.count)
-            let radius = caseCount/10 * 100
-            return radius
+exports.getRadius = function(suburbCases, suburb){
+    let radius = 0
+
+    for(var i=0; i<suburbCases.length; i++) {
+        if(suburb.name === suburbCases[i].name){
+            let caseCount = getIntegerCases(suburbCases[i].count)
+            radius = caseCount/10 * 300
+            break
         }
-    })
+    }
+
+    return radius
 }
 
 //Parses the count field
-exports.getIntegerCases = function(caseCount){
+function getIntegerCases(caseCount){
     if(caseCount.indexOf('-') > -1){
         //1-4 type of caseCount
         return Number(caseCount.split("-")[0])
