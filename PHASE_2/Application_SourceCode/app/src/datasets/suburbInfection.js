69c83ff4a122f0f07902b0a376fc0b796a0b7e8e
Ravija Maheshwari <iamravija@yahoo.com>
2020-04-04 18:03:49 +1100

    Suburb infection static datset

---
+++ PHASE_2/Application_SourceCode/app/src/datasets/suburbInfection.js
@@ -0,0 +1,398 @@
+exports.suburbInfection = [
+    {
+        "count": "6",
+        "name": "Albury"
+    },
+    {
+        "count": "1-4",
+        "name": "Armidale Regional"
+    },
+    {
+        "count": "1-4",
+        "name": "Ballina"
+    },
+    {
+        "count": "9",
+        "name": "Bathurst Regional"
+    },
+    {
+        "count": "39",
+        "name": "Bayside"
+    },
+    {
+        "count": "9",
+        "name": "Bega Valley"
+    },
+    {
+        "count": "1-4",
+        "name": "Bellingen"
+    },
+    {
+        "count": "1-4",
+        "name": "Berrigan"
+    },
+    {
+        "count": "85",
+        "name": "Blacktown"
+    },
+    {
+        "count": "1-4",
+        "name": "Blayney"
+    },
+    {
+        "count": "21",
+        "name": "Blue Mountains"
+    },
+    {
+        "count": "1-4",
+        "name": "Broken Hill"
+    },
+    {
+        "count": "7",
+        "name": "Burwood"
+    },
+    {
+        "count": "13",
+        "name": "Byron"
+    },
+    {
+        "count": "1-4",
+        "name": "Cabonne"
+    },
+    {
+        "count": "29",
+        "name": "Camden"
+    },
+    {
+        "count": "27",
+        "name": "Campbelltown"
+    },
+    {
+        "count": "16",
+        "name": "Canada Bay"
+    },
+    {
+        "count": "77",
+        "name": "Canterbury-Bankstown"
+    },
+    {
+        "count": "98",
+        "name": "Central Coast"
+    },
+    {
+        "count": "24",
+        "name": "Cessnock"
+    },
+    {
+        "count": "8",
+        "name": "Clarence Valley"
+    },
+    {
+        "count": "11",
+        "name": "Coffs Harbour"
+    },
+    {
+        "count": "1-4",
+        "name": "Coonamble"
+    },
+    {
+        "count": "1-4",
+        "name": "Cootamundra-Gundagai Regional"
+    },
+    {
+        "count": "52",
+        "name": "Cumberland"
+    },
+    {
+        "count": "5",
+        "name": "Dubbo Regional"
+    },
+    {
+        "count": "5",
+        "name": "Dungog"
+    },
+    {
+        "count": "9",
+        "name": "Eurobodalla"
+    },
+    {
+        "count": "14",
+        "name": "Fairfield"
+    },
+    {
+        "count": "7",
+        "name": "Federation"
+    },
+    {
+        "count": "40",
+        "name": "Georges River"
+    },
+    {
+        "count": "1-4",
+        "name": "Glen Innes Severn"
+    },
+    {
+        "count": "8",
+        "name": "Goulburn Mulwaree"
+    },
+    {
+        "count": "1-4",
+        "name": "Greater Hume Shire"
+    },
+    {
+        "count": "1-4",
+        "name": "Griffith"
+    },
+    {
+        "count": "1-4",
+        "name": "Gunnedah"
+    },
+    {
+        "count": "24",
+        "name": "Hawkesbury"
+    },
+    {
+        "count": "1-4",
+        "name": "Hay"
+    },
+    {
+        "count": "1-4",
+        "name": "Hilltops"
+    },
+    {
+        "count": "43",
+        "name": "Hornsby"
+    },
+    {
+        "count": "14",
+        "name": "Hunters Hill"
+    },
+    {
+        "count": "59",
+        "name": "Inner West"
+    },
+    {
+        "count": "1-4",
+        "name": "Inverell"
+    },
+    {
+        "count": "1-4",
+        "name": "Kempsey"
+    },
+    {
+        "count": "5",
+        "name": "Kiama"
+    },
+    {
+        "count": "55",
+        "name": "Ku-ring-gai"
+    },
+    {
+        "count": "1-4",
+        "name": "Lachlan"
+    },
+    {
+        "count": "40",
+        "name": "Lake Macquarie"
+    },
+    {
+        "count": "29",
+        "name": "Lane Cove"
+    },
+    {
+        "count": "5",
+        "name": "Lismore"
+    },
+    {
+        "count": "1-4",
+        "name": "Lithgow"
+    },
+    {
+        "count": "34",
+        "name": "Liverpool"
+    },
+    {
+        "count": "1-4",
+        "name": "Liverpool Plains"
+    },
+    {
+        "count": "27",
+        "name": "Maitland"
+    },
+    {
+        "count": "35",
+        "name": "Mid-Coast"
+    },
+    {
+        "count": "5",
+        "name": "Mid-Western Regional"
+    },
+    {
+        "count": "31",
+        "name": "Mosman"
+    },
+    {
+        "count": "1-4",
+        "name": "Murrumbidgee"
+    },
+    {
+        "count": "1-4",
+        "name": "Muswellbrook"
+    },
+    {
+        "count": "1-4",
+        "name": "Nambucca"
+    },
+    {
+        "count": "1-4",
+        "name": "Narrandera"
+    },
+    {
+        "count": "1-4",
+        "name": "Narromine"
+    },
+    {
+        "count": "47",
+        "name": "Newcastle"
+    },
+    {
+        "count": "42",
+        "name": "North Sydney"
+    },
+    {
+        "count": "130",
+        "name": "Northern Beaches"
+    },
+    {
+        "count": "7",
+        "name": "Orange"
+    },
+    {
+        "count": "40",
+        "name": "Parramatta"
+    },
+    {
+        "count": "54",
+        "name": "Penrith"
+    },
+    {
+        "count": "27",
+        "name": "Port Macquarie-Hastings"
+    },
+    {
+        "count": "30",
+        "name": "Port Stephens"
+    },
+    {
+        "count": "9",
+        "name": "Queanbeyan-Palerang Regional"
+    },
+    {
+        "count": "58",
+        "name": "Randwick"
+    },
+    {
+        "count": "58",
+        "name": "Ryde"
+    },
+    {
+        "count": "19",
+        "name": "Shellharbour"
+    },
+    {
+        "count": "27",
+        "name": "Shoalhaven"
+    },
+    {
+        "count": "1-4",
+        "name": "Singleton"
+    },
+    {
+        "count": "6",
+        "name": "Snowy Monaro Regional"
+    },
+    {
+        "count": "5",
+        "name": "Strathfield"
+    },
+    {
+        "count": "70",
+        "name": "Sutherland Shire"
+    },
+    {
+        "count": "122",
+        "name": "Sydney"
+    },
+    {
+        "count": "12",
+        "name": "Tamworth Regional"
+    },
+    {
+        "count": "1-4",
+        "name": "Tenterfield"
+    },
+    {
+        "count": "53",
+        "name": "The Hills Shire"
+    },
+    {
+        "count": "12",
+        "name": "Tweed"
+    },
+    {
+        "count": "1-4",
+        "name": "Upper Hunter Shire"
+    },
+    {
+        "count": "1-4",
+        "name": "Upper Lachlan Shire"
+    },
+    {
+        "count": "1-4",
+        "name": "Uralla"
+    },
+    {
+        "count": "10",
+        "name": "Wagga Wagga"
+    },
+    {
+        "count": "1-4",
+        "name": "Warren"
+    },
+    {
+        "count": "1-4",
+        "name": "Warrumbungle Shire"
+    },
+    {
+        "count": "156",
+        "name": "Waverley"
+    },
+    {
+        "count": "1-4",
+        "name": "Weddin"
+    },
+    {
+        "count": "21",
+        "name": "Willoughby"
+    },
+    {
+        "count": "18",
+        "name": "Wingecarribee"
+    },
+    {
+        "count": "1-4",
+        "name": "Wollondilly"
+    },
+    {
+        "count": "48",
+        "name": "Wollongong"
+    },
+    {
+        "count": "84",
+        "name": "Woollahra"
+    },
+    {
+        "count": "6",
+        "name": "Yass Valley"
+    }
+]
