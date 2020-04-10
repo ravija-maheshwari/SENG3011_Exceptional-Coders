d9d93cd2db965af00092c0f01369df42cbe660de
Ravija Maheshwari <iamravija@yahoo.com>
2020-04-11 00:04:23 +1000

    current points plotted on graph

--- PHASE_2/Application_SourceCode/app/src/components/graph.js
+++ PHASE_2/Application_SourceCode/app/src/components/graph.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { graphData } from '../datasets/graphTestData'
+import { suburbInfection } from '../datasets/suburbInfection'
 import Chart from "chart.js"
+import regression from "regression"
 
 class Graph extends React.Component{
     constructor(props) {
@@ -10,24 +11,56 @@ class Graph extends React.Component{
 
     getMatchingData(suburb){
         let allData = []
-        graphData.forEach(doc => {
-            if(doc.name.includes(suburb)){
+        suburbInfection.forEach(doc => {
+            if(doc.name === suburb){
                 allData.push(doc)
             }
         })
         return allData
     }
 
+    getIntegerDate(datePassed){
+        let startingDate = new Date("2020-04-06")
+        let date = new Date(datePassed)
+        let diffDay = (date - startingDate)/(1000*60*60*24)
+        return diffDay
+    }
+
+    getIntegerCases(caseCount){
+        if(caseCount.indexOf('-') > -1){
+            //1-4 type of caseCount
+            return Number(caseCount.split("-")[0])
+        }else{
+            return Number(caseCount)
+        }
+    }
+
+    //Confirmed cases co-ordinates
+    getCurrentPoints(){
+        let points = []
+        let matchingSuburbs = this.getMatchingData(this.props.suburb)
+        for(var i = 0; i < matchingSuburbs.length; i++){
+            let date = this.getIntegerDate(matchingSuburbs[i].date)
+            let count = this.getIntegerCases(matchingSuburbs[i].count)
+            let dateString = matchingSuburbs[i].date
+            points.push( [dateString, date, count] )
+        }
+        return points
+    }
+
+
     componentDidMount() {
+
         const node = this.node
-        let data = this.getMatchingData(this.props.suburb)
+        // let data = this.getMatchingData(this.props.suburb)
+        let currentPoints = this.getCurrentPoints()
         this.myChart = new Chart(node, {
             type: 'line',
             data: {
-                labels: data.map(obj => obj.date),
+                labels: currentPoints.map(obj => obj[0]),
                 datasets: [{
                     label: "cases in " + this.props.suburb,
-                    data: data.map(obj => obj.count),
+                    data: currentPoints.map(obj => obj[2]),
                     backgroundColor: "#70CAD1"
                 }]
             }
